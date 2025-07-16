7744aca58a44f3f98c1d093af0512c0327eb504d
Jshebb <joaohsebbers@gmail.com>
2025-07-16 19:49:14 -0300

    feature: Data persistence

--- backend/data-storage-service/models/schemas.py
+++ backend/data-storage-service/models/schemas.py
@@ -19,3 +19,24 @@ class UserResponse(BaseModel):
     
     class Config:
         from_attributes = True
+
+class MarketData (BaseModel):
+    id: int | None = None  # Torna opcional
+    nome: str
+    ticker: str
+    data_inicio: date
+    data_fim: date
+    open_price: float
+    high_price: float
+    low_price: float
+    close_price: float
+    volume: int
+    symbol: str
+    shortName: str
+    longName: str
+    sector: str
+    industry: str
+    exchange: str
+
+    class Config:
+        from_attributes = True
