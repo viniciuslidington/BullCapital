f08b1c6f5bef7c0e0bd543e595e937cf6468cb7e
Cadu Falcão <kddu422@gmail.com>
2025-07-17 22:34:46 -0300

    Header e Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 import { ThemeProvider } from "./contexts/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Header } from "./components/ui/header";
+import { Footer } from "./components/ui/footer";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <BrowserRouter>
-          <Routes>
-            <Route></Route>
-          </Routes>
-        </BrowserRouter>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <div className="h-screen max-h-screen w-full flex-col">
+          <Header />
+
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+            </Routes>
+          </BrowserRouter>
+          <Footer />
+        </div>
       </ThemeProvider>
     </>
   );
