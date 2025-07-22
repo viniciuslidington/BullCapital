2b8eb2db54be638d614294bd968955cf970645a3
Jshebb <joaohsebbers@gmail.com>
2025-07-22 20:32:18 -0300

    feat: New endpoints

--- backend/gateway-service/models/responses/market_data_response.py
+++ backend/gateway-service/models/responses/market_data_response.py
@@ -1,7 +1,16 @@
-StockDataResponse = {
-    "symbol": str,
-    "price": float,
-    "volume": int,
-    "market_cap": float,
-    "pe_ratio": float,
-}
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+
+class StockDataResponse(BaseModel):
+    symbol: str
+    company_name: str
+    current_price: float
+    previous_close: float
+    change: float
+    change_percent: float
+    volume: int
+    avg_volume: Optional[int] = None # avg_volume é null no JSON, então Optional
+    currency: str
+    timezone: Optional[str] = None # timezone é null no JSON, então Optional
+    last_updated: str # Pode ser datetime.date ou datetime.datetime se você quiser parsear datas
