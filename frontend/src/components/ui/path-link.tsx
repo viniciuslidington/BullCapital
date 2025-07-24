a02220dd0c763b42cd8bf31e0c06b01999eb873c
Cadu Falcão <kddu422@gmail.com>
2025-07-24 13:09:31 -0300

    pathlink component

---
+++ frontend/src/components/ui/path-link.tsx
@@ -0,0 +1,47 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export function PathLink() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <Breadcrumb>
+      <BreadcrumbList className="text-primary/70 text-base font-medium">
+        <BreadcrumbItem>
+          <BreadcrumbLink
+            className="hover:text-primary cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            Home
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        {location.state?.from && (
+          <>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                className="cursor-pointer"
+                onClick={() => navigate(-1)}
+              >
+                {location.state.from}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </>
+        )}
+        <BreadcrumbItem>
+          <BreadcrumbPage className="text-primary cursor-pointer font-medium">
+            Breadcrumb
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
---
+++ frontend/src/pages/highlight.tsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom";
+
+export function Highlight() {
+  return <Link to={"/AAPL"}>ativo</Link>;
+}
