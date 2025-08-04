a6d66374467b2b6ec8cec5f11e6c52bc49dd6fce
Cadu Falcão <kddu422@gmail.com>
2025-08-04 10:02:59 -0300

    setup dependencias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "./contexts/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Home } from "./pages/home";
 import { Header } from "./components/ui/header";
 import { Footer } from "./components/ui/footer";
@@ -8,10 +10,13 @@ import { Asset } from "./pages/asset";
 import { Ranking } from "./pages/ranking";
 import { ScrollToTop } from "./components/ui/scrolltotop";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <ScrollToTop />
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <div className="min-h-screen w-full flex-col">
@@ -30,8 +35,8 @@ function App() {
             </div>
           </div>
         </ThemeProvider>
-      </BrowserRouter>
-    </>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
