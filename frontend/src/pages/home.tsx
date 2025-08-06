a4d5e5d5f4f4fdb7f5b44b2b1d758b1f4e2cc0c6
Cadu Falcão <kddu422@gmail.com>
2025-08-06 14:59:13 -0300

    melhoras no layout

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,13 +2,28 @@ import { AssetChart } from "@/components/features/assets-chart/asset-chart";
 import { HighlightsOverview } from "@/components/features/highlights-overview/highlights-overview";
 import { MarketOverview } from "@/components/features/market-overview/market-overview";
 import { ChartTabs } from "@/components/features/markets-chart/chart-tabs";
+import { Button } from "@/components/ui/button";
 import { SearchBar } from "@/components/ui/search-bar";
+import { ChartNoAxesCombined } from "lucide-react";
 
 export function Home() {
   return (
     <div className="flex h-auto w-full max-w-[1180px] flex-col gap-8 p-8">
       <MarketOverview />
-      <SearchBar />
+      <div className="flex flex-col items-center gap-16 pt-10 pb-16">
+        <h1 className="text-gradient bg-gradient-to-tr from-slate-600 via-slate-400 to-slate-50 text-center text-4xl font-semibold">
+          Eleve seu nível como os grandes investidores <br /> e tome decisões
+          com confiança
+        </h1>
+        <SearchBar />
+        <Button
+          variant="outline"
+          className="cursor-pointer rounded-3xl text-xs"
+        >
+          <ChartNoAxesCombined></ChartNoAxesCombined>
+          Ver Ranking de ativos
+        </Button>
+      </div>
       <HighlightsOverview />
       <ChartTabs />
       <AssetChart />
